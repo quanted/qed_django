67382a7499a33ca0a787e7f685267eeb06a85f5a
Deron Smith <deron.brock@gmail.com>
2021-08-16 11:52:28 -0500

    Update settings_docker.py

--- settings_docker.py
+++ settings_docker.py
@@ -13,10 +13,28 @@ import os
 import socket
 from settings import *
 from django.conf import settings
-# import settings
-# from . import settings
+from temp_config.set_environment import DeployEnv
+
+# Determine env vars to use:
+runtime_env = DeployEnv()
+runtime_env.load_deployment_environment()
 
 print('settings_docker.py')
+IN_PROD = (os.getenv("IN_PROD") == "1")
+print("Production Deployment: {}".format(IN_PROD))
+if IN_PROD:
+    DEBUG = False
+    IS_PUBLIC = True
+    CORS_ORIGIN_ALLOW_ALL = False
+    PUBLIC_APPS = ['cts', 'hms', 'pisces', 'cyanweb']
+    PASSWORD_REQUIRED = False
+    os.environ.update({'HMS_RELEASE': 'True'})
+else:
+    DEBUG = True
+    IS_PUBLIC = False
+    CORS_ORIGIN_ALLOW_ALL = True
+    PASSWORD_REQUIRED = True
+    PUBLIC_APPS = ['cts', 'hms', 'pisces', 'cyan', 'pram']
 
 # Get machine IP address
 MACHINE_ID = socket.gethostname()
@@ -30,26 +48,8 @@ os.environ.update({
     'PROJECT_PATH': PROJECT_ROOT,
     'SITE_SKIN': 'EPA',  # Leave empty ('') for default skin, 'EPA' for EPA skin
     'CONTACT_URL': 'https://www.epa.gov/research/forms/contact-us-about-epa-research',
-    # # # cts_api addition:
-    # 'CTS_EPI_SERVER': 'http://172.20.100.18',
-    # 'CTS_EFS_SERVER': 'http://172.20.100.12',
-    # 'CTS_JCHEM_SERVER': 'http://172.20.100.12',
-    # 'CTS_SPARC_SERVER': 'http://204.46.160.69:8080',
-    # 'CTS_TEST_SERVER': 'http://172.20.100.16:8080',
-    # 'CTS_REST_SERVER': 'http://172.20.100.11', #using qedinternal as proxy for rest server
-    # # 'CTS_VERSION': '1.8'  # Now set at settings.py
 })
 
-# SECURITY WARNING: don't run with debug turned on in production! (unless you are desperate)
-# DEBUG = False
-if not os.environ.get('IS_PUBLIC'):
-    DEBUG = True
-else:
-    if os.environ.get('IS_PUBLIC') == "True":
-        DEBUG = False
-    else:
-        DEBUG = True
-print("DEBUG: " + str(DEBUG))
 TEMPLATE_DEBUG = False
 
 if not os.environ.get('UBERTOOL_REST_SERVER'):
@@ -71,6 +71,7 @@ except IOError as e:
 
 try:
     HOSTNAME = os.environ.get('DOCKER_HOSTNAME')
+    #IS_PUBLIC = (os.environ.get('IS_PUBLIC') == "True")
     # with open('secret_key_django_dropbox.txt') as f:
     #        SECRET_KEY = f.read().strip()
 except IOError as e:
@@ -84,24 +85,33 @@ except IOError as e:
 #    print("No IP address given")
 #    IP_ADDRESS = '0.0.0.0'
 
+
 ALLOWED_HOSTS = []
-if HOSTNAME == "ord-uber-vm003":
-    IS_PUBLIC = True
-else:
-    IS_PUBLIC = False
-if HOSTNAME == "ord-uber-vm001":
-    ALLOWED_HOSTS.append('qedinternal.epa.gov')
+
+if IS_PROD:
+    ALLOWED_HOSTS.append('134.67.114.3')  # CGI NAT address (mapped to 'qed.epa.gov')
     ALLOWED_HOSTS.append('134.67.114.1')
+    ALLOWED_HOSTS.append('134.67.114.5')
     ALLOWED_HOSTS.append('172.20.100.11')
-elif HOSTNAME == "ord-uber-vm003":
-    ALLOWED_HOSTS.append('134.67.114.3')
     ALLOWED_HOSTS.append('172.20.100.13')
+    ALLOWED_HOSTS.append('172.20.100.15')
     ALLOWED_HOSTS.append('qed.epa.gov')
+    ALLOWED_HOSTS.append('qed.edap-cluster.com')
+    ALLOWED_HOSTS.append('ceamdev.ddns.net')
+    ALLOWED_HOSTS.append('ceamstg.ddns.net')
+    ALLOWED_HOSTS.append('ceamdev.ceeopdev.net')
+    ALLOWED_HOSTS.append('ceamstg.ceeopdev.net')
+    ALLOWED_HOSTS.append('qedlinux1dev.aws.epa.gov')
+    ALLOWED_HOSTS.append('qedlinux1stg.aws.epa.gov')
+    ALLOWED_HOSTS.append('awqedlinprd.aws.epa.gov')
 elif HOSTNAME == "UberTool-Dev":
     ALLOWED_HOSTS.append('172.16.0.4')
-elif "KUBE" in HOSTNAME:
-    ALLOWED_HOSTS.append('*')
+    ALLOWED_HOSTS.append('qed.epacdx.net')
 else:
+    ALLOWED_HOSTS.append('localhost')
+    ALLOWED_HOSTS.append('127.0.0.1')
+    ALLOWED_HOSTS.append('host.docker.internal')
+    ALLOWED_HOSTS.append('192.168.99.100')  # Docker Machine IP (generally, when using VirtualBox VM)
     ALLOWED_HOSTS.append('134.67.114.3')  # CGI NAT address (mapped to 'qed.epa.gov')
     ALLOWED_HOSTS.append('134.67.114.1')
     ALLOWED_HOSTS.append('134.67.114.5')
@@ -110,23 +120,66 @@ else:
     ALLOWED_HOSTS.append('172.20.100.15')
     ALLOWED_HOSTS.append('qedinternal.epa.gov')
     ALLOWED_HOSTS.append('qed.epa.gov')
-
-local_docker = os.environ.get("LOCAL_DOCKER")
-print("LOCAL_DOCKER = {}".format(local_docker))
-if local_docker == "True":
-    ALLOWED_HOSTS.append('*')
-    print("WARNING: Allowed hosts includes '*', should only be used in secure testing environment.")
-
+    ALLOWED_HOSTS.append('qedinternalblue.edap-cluster.com')
+    ALLOWED_HOSTS.append('qedinternal.edap-cluster.com')
+    ALLOWED_HOSTS.append('qed.edap-cluster.com')
+    ALLOWED_HOSTS.append('qedblue.edap-cluster.com')
+    ALLOWED_HOSTS.append('ceamdev.ddns.net')
+    ALLOWED_HOSTS.append('ceamstg.ddns.net')
+    ALLOWED_HOSTS.append('ceamdev.ceeopdev.net')
+    ALLOWED_HOSTS.append('ceamstg.ceeopdev.net')
+    ALLOWED_HOSTS.append('qedlinux1dev.aws.epa.gov')
+    ALLOWED_HOSTS.append('qedlinux1stg.aws.epa.gov')
+    ALLOWED_HOSTS.append('awqedlinprd.aws.epa.gov')
 
 print("MACHINE_ID = {}".format(MACHINE_ID))
 print("HOSTNAME = {}".format(HOSTNAME))
 print("IS_PUBLIC = {}".format(IS_PUBLIC))
 
+# Application definition
+if IN_PROD:
+    INSTALLED_APPS = (
+        'corsheaders',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'cts_app',  # cts django app
+        'cts_app.filters',  # cts filters for pchem table
+        'cts_app.cts_api',
+        'cts_app.cts_testing',
+        'hms_app',  # hms django app
+        'pisces_app',  # pisces django app
+        'splash_app',  # splash django app
+    )
+else:
+    INSTALLED_APPS = (
+        'corsheaders',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'cts_app',  # cts django app
+        'cts_app.filters',  # cts filters for pchem table
+        'cts_app.cts_api',
+        'cts_app.cts_testing',
+        'cyan_app',  # cyan django app
+        'hms_app',  # hms django app
+        'hwbi_app',  # hwbi django app
+        'nta_app',
+        'pisces_app',  # pisces django app
+        'pram_app',  # pram django app
+        'splash_app',  # splash django app
+    )
+
 # Disable this because Django wants to email errors and there is no email server set up
 # ADMINS = (
 #     ('Ubertool Dev Team', 'ubertool-dev@googlegroups.com')
 # )
-
 
 MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -134,14 +187,8 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware', #rollbar
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware', #rollbar
-    # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # 'login_middleware.RequireLoginMiddleware',
 ]
-# if os.environ.get('IS_PUBLIC') == "True":
-#     MIDDLEWARE_CLASSES += ['login_middleware.RequireLoginMiddleware',]
-
 
 ROOT_URLCONF=__name__, #rollbar
 
@@ -160,27 +207,6 @@ ROLLBAR = {
     'root': os.getcwd()
 }
 
-## rollbar integration
-#settings.configure(
-#    DEBUG=True,
-#    SECRET_KEY='thisisthesecretkey',
-#    ROOT_URLCONF=__name__,
-#    ROLLBAR = {
-#        'access_token': 'POST_SERVER_ITEM_ACCESS_TOKEN',
-#        'environment': 'development',
-#        'branch': 'master',
-#        'root': os.getcwd()
-#    },
-#    MIDDLEWARE_CLASSES=(
-#        'django.middleware.common.CommonMiddleware',
-#        'django.middleware.csrf.CsrfViewMiddleware',
-#        'django.middleware.clickjacking.XFrameOptionsMiddleware',###
-#
-#        # Rollbar middleware
-#        'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
-#    ),
-#)
-
 ROOT_URLCONF = 'urls'
 
 WSGI_APPLICATION = 'wsgi_docker.application'
@@ -191,12 +217,15 @@ WSGI_APPLICATION = 'wsgi_docker.application'
 # Authentication
 AUTH = False
 # Note: env vars in os.environ always strings..
-# if os.environ.get('IS_PUBLIC') == "True":
-if IS_PUBLIC:
-    logging.warning("IS_PUBLIC set to true..")
-    MIDDLEWARE += ['login_middleware.RequireLoginMiddleware',]
+if PASSWORD_REQUIRED:
+    logging.warning("Password protection enabled")
+    MIDDLEWARE += [
+        'login_middleware.RequireLoginMiddleware',
+        'login_middleware.Http403Middleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ]
     AUTH = True
-    DEBUG = False
+    # DEBUG = False
 
 REQUIRE_LOGIN_PATH = '/login/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -214,7 +243,23 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = '/src/collected_static/'
+# STATIC_ROOT = os.path.join('src', 'collected_static')
+STATIC_ROOT = os.path.join('collected_static')
+HMS_ANGULAR_APP_DIR = "/src/static_qed/hms/webapp"
+HMS_ANGULAR_APP_ASSETS_DIR = "/src/static_qed/hms/webapp/assets"
+CYANWEB_ANGULAR_APP_DIR = "/src/static_qed/epa-cyano-web"
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static_qed'),
+    # os.path.join(PROJECT_ROOT, 'collected_static'),
+    # HMS_ANGULAR_APP_DIR,
+    # CYANWEB_ANGULAR_APP_DIR
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+STATIC_URL = '/static_qed/'
 
 # Log to console in Debug mode
 if DEBUG:
@@ -222,3 +267,4 @@ if DEBUG:
         level=logging.DEBUG,
         format='%(asctime)s %(levelname)s %(message)s',
     )
+print(f"Staticfiles: {STATICFILES_DIRS}")
